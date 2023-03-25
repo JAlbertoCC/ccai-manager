89021a298f366b9488928d3b05bfbc9f46ea8fe2
Sandy Cortes <sandra_hakufu@hotmail.com>
2023-03-24 21:18:49 -0600

    set new styles in select

--- src/components/ui/DropDown/DropDown.js
+++ src/components/ui/DropDown/DropDown.js
@@ -4,29 +4,22 @@ export const DropDown = (props) => {
     const {seleccion=[], items = [],  title, iconName,typeSelect, typeInput, hdlOnClick, classExtra, hdlOnkeyDown, hdlOnChange, item1,item2,item3,item4} = props
 
     return (
-        <label className="float-label">
-        <div className="select">
+      <label className="float-label">
+        <div className="select is-rounded">
         
-        <select
+          <select
             onChange={hdlOnChange}
-            className={`select-width input-radious ${classExtra}`}
-            >
-        {items ?
-            items.map(item => {
-              return <option key={item.id} value={item.id}>{item.name}</option>
-            }) : <></>
-        }
-
-        </select>
-        <span className='float-span'>{ title }</span>
-            
-            
-            
-        </div>
-        
-            
-        
-        </label>
-    )
+            className={`select-width input-radious select-style ${classExtra}`}
+          >
+            {items ?
+              items.map(item => {
+                return <option key={item.id} value={item.id}>{item.name}</option>
+              }) : <></>
+            }
+          </select>
+          <span className='float-span'>{ title }</span>   
+        </div>  
+      </label>
+    );
 };
 
