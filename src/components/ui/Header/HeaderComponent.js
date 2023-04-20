43f90acc9bd3d04409c551b3142ca4f49b23e3bd
Sandy Cortes <sandra_hakufu@hotmail.com>
2023-04-20 12:57:00 -0600

    change icons for home view

--- src/components/ui/Header/HeaderComponent.js
+++ src/components/ui/Header/HeaderComponent.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+
 export const HeaderComponent = (props) => {
+  const navigate = useNavigate();
+  
+  const goToLink = () => {
+    navigate(-1);
+  };
+  
   return (
-      <div className="header-componet">     
-        <a href="/">
-          <i className="mdi mdi-chevron-left"></i> {props.title}   
+      <div className="header-componet">        
+        <a onClick={() => goToLink()}>
+        <i className="mdi mdi-chevron-left" /> {props.title}
         </a> 
       </div>    
   )
