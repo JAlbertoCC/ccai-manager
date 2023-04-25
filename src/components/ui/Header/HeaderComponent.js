f0213caf2c7f7560c0f342e6fb32d38525de597b
Sandy Cortes <sandra_hakufu@hotmail.com>
2023-04-24 22:50:50 -0600

    fix in register user

--- src/components/ui/Header/HeaderComponent.js
+++ src/components/ui/Header/HeaderComponent.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useNavigate  } from 'react-router-dom'
+
 export const HeaderComponent = (props) => {
+  const navigate = useNavigate()
+
   return (
       <div className="header-componet">     
-        <a href="/">
+        <a onClick={() => navigate(-1)}>
           <i className="mdi mdi-chevron-left"></i> {props.title}   
         </a> 
       </div>    
