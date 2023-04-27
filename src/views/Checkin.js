a347ad106a40f4d4bb6040c7f9f14f6ca3ec2f54
Sandy Cortes <sandra_hakufu@hotmail.com>
2023-04-27 15:24:57 -0600

    set rule for register

--- src/views/Checkin.js
+++ src/views/Checkin.js
@@ -5,7 +5,7 @@ import { InputLabel } from "../components/ui/Inputs/InputLabel";
 import { ErrorMessage } from "../components/ui/Warnings/ErrorMessage";
 import { ButtonComponent } from "../components/ui/Buttons/PrimaryButton";
 
-import { useNavigate  } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 import { DateTime } from "luxon";
 import { useUsers } from "./../hooks/useUsers";
@@ -17,104 +17,120 @@ const Checkin = () => {
   const [userInformation, setUserInformation] = useState({});
   const [showError, setShowError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [isLoader, setIsLoader] = useState(false);
 
-
   const goToLink = (uri) => {
-    navigate(uri)
-  }
+    navigate(uri);
+  };
 
   useEffect(() => {
-    console.log(DateTime.now().toLocaleString(DateTime.DATE_MED))
-  }, [showComponets])
-  
+    console.log(DateTime.now().toLocaleString(DateTime.DATE_MED));
+  }, [showComponets]);
+
   const render = (value) => {
-      checkingInternalUser(value.target.value)
-        .then(item => {
-          setIsLoader(false)
-          if (item?.result?.matricula_student) {
-            setErrorMessage(item?.result?.matricula_student);
-            setShowError(!showError);
-          } else if (item) { 
-            setShowError(false);
-            setUserInformation(item?.result);
-            setShowComponets(!showComponets);
-            setTimeout(() => {
-              window.location.reload();
-            }, 5000);
-          }
-        })
-        .catch(error => {
-          setIsLoader(false)
-          setShowComponets(false)
-          console.log('error', error.message)
-          setErrorMessage(error.message);
-          setShowError(true);
-        });
-  }
-  
+    checkingInternalUser(value.target.value)
+      .then((item) => {
+        setIsLoader(false);
+        if (item?.result?.matricula_student) {
+          setErrorMessage(item?.result?.matricula_student);
+          setShowError(!showError);
+        } else if (item) {
+          setShowError(false);
+          setUserInformation(item?.result);
+          setShowComponets(!showComponets);
+          setTimeout(() => {
+            window.location.reload();
+          }, 5000);
+        }
+      })
+      .catch((error) => {
+        setIsLoader(false);
+        setShowComponets(false);
+        console.log("error", error.message);
+        setErrorMessage(error.message);
+        setShowError(true);
+      });
+  };
+
   const getDate = (type) => {
-    return DateTime.now().toLocaleString(DateTime[type])
+    return DateTime.now().toLocaleString(DateTime[type]);
   };
   return (
     <div className="section">
       {isLoader && <ModalComponent />}
-      {
-        showComponets ? (
-          <CardComponent classExtra="opacity-card card-check">
-            <p className="title-sucessfull">Bienvenido al CCAI </p>
-            <figure className="image is-96x96 center-img">
-              <img src={require("./../assets/logo.png")} alt="" />
-            </figure>
+      {showComponets ? (
+        <CardComponent classExtra="opacity-card card-check">
+          <p className="title-sucessfull">Bienvenido al CCAI </p>
+          <figure className="image is-96x96 center-img">
+            <img src={require("./../assets/logo.png")} alt="" />
+          </figure>
 
-            <br />
+          <br />
 
-            <p className="dialog-view-check"> Agradecemos su visita, </p>
+          <p className="dialog-view-check"> Agradecemos su visita, </p>
+          <br />
+          <p className="dialog-view-check">
+            {" "}
+            Por favor, registre sus accesos y salidas en el lector.{" "}
+          </p>
+          <br />
+          <InputLabel
+            typeInput="text"
+            classExtra="input-check"
+            isEnter={true}
+            hdlOnkeyDown={(e) => render(e)}
+          />
+          <br />
+          <p className="dialog-view-check">
+            {" "}
+            Si eres visitante, pulsa el siguiente botón.{" "}
+          </p>
+          <br />
+          <ButtonComponent
+            buttonText="Visitante"
+            hdlOnClickEvent={() => goToLink("/visit-view")}
+          />
+        </CardComponent>
+      ) : (
+        <CardComponent classExtra="opacity-card card-check">
+          <div className="container-dates">
+            <p className="text-date">{getDate("DATE_MED")}</p>
             <br />
-            <p className="dialog-view-check"> Por favor, registre sus accesos y salidas en el lector. </p>
-            <br />
-            <InputLabel
-              typeInput="text"
-              classExtra="input-check"
-              isEnter={true}
-              hdlOnkeyDown={(e) => render(e)}
-            />
-            <br />
-            <p className="dialog-view-check"> Si eres visitante, pulsa el siguiente botón. </p>
-            <br />
-            <ButtonComponent buttonText="Visitante" hdlOnClickEvent={() => goToLink('/visit-view')} />
-          </CardComponent>
-        ) : (
-          <CardComponent classExtra="opacity-card card-check">
-            <div className="container-dates">
-              <p className='text-date'>{ getDate('DATE_MED') }</p>
-              <br />
-              <div className='container-hours'>
-                <div className='text-hour'>{ getDate('TIME_24_SIMPLE') }</div>
-              </div>
+            <div className="container-hours">
+              <div className="text-hour">{getDate("TIME_24_SIMPLE")}</div>
             </div>
-            <br />
-            <p className="dialog-view-check"> Bienvenido {`${userInformation.name} ${userInformation.first_name} ${userInformation.second_name}`} </p>
-            <br />
-            <p className="dialog-view-check"> Se ha registrado correctamente tu entrada a las {getDate('TIME_24_SIMPLE')}. </p>
-            <br />
-            <p className="text-alert">  No olvides registrar en el lector tus entradas y salidas del CCAI </p>
-          </CardComponent>
-        )
-      }
+          </div>
+          <br />
+          <p className="dialog-view-check">
+            {" "}
+            Bienvenido{" "}
+            {`${userInformation.name} ${userInformation.first_name} ${userInformation.second_name}`}{" "}
+          </p>
+          <br />
+          <p className="dialog-view-check">
+            {" "}
+            Se ha registrado correctamente tu entrada a las{" "}
+            {getDate("TIME_24_SIMPLE")}.{" "}
+          </p>
+          <br />
+          <p className="text-alert">
+            {" "}
+            No olvides registrar en el lector tus entradas y salidas del CCAI{" "}
+          </p>
+        </CardComponent>
+      )}
 
-      {
-        showError ?
-          <ErrorMessage
-            message={errorMessage}
-            hdlOnClick={() => setShowError(!showError)}
-          
-          /> : <></>
-      }
+      {showError ? (
+        <ErrorMessage
+          message={errorMessage}
+          hdlOnClick={() => setShowError(!showError)}
+        />
+      ) : (
+        <></>
+      )}
     </div>
   );
-
 };
 
 export default Checkin;
