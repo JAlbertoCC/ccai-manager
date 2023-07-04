a58a940a414bde012ea66a548d9d8747d6005550
Wendy Cain <wencain09@gmail.com>
2023-07-04 16:34:42 -0600

    vista de calendario

---
+++ src/components/ui/Calendars/PrimaryCalendar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+//import bulmaCalendar from 'bulma-calendar';
+
+export const PrimaryCalendar = (props) => {
+    return (
+       <div class= "calendar">
+        <div class ="calendar-nav">
+            <div class = "calendar-nav-left">
+            </div>
+            <div>Julio 2023</div>
+            <div class="calendar-nav-rigth">
+            </div>
+        </div>
+        <div class="calendar-container">
+            <div class="calendar-hearder">
+            <div class="calendar-date">Dom</div>
+            <div class="calendar-date">Lun</div>
+            <div class="calendar-date">Mar</div>
+            <div class="calendar-date">Mie</div>
+            <div class="calendar-date">Jue</div>
+            <div class="calendar-date">Vie</div>
+            <div class="calendar-date">Sab</div>
+            </div>
+            <div class="calendar-body">
+                <div class="calendar-date is-disbled"><button class="date-item">25</button></div>
+                <div class="calendar-date is-disbled"><button class="date-item">26</button></div>
+                <div class="calendar-date is-disbled"><button class="date-item">27</button></div>
+                <div class="calendar-date is-disbled"><button class="date-item">28</button></div>
+                <div class="calendar-date is-disbled"><button class="date-item">29</button></div>
+                <div class="calendar-date is-disbled"><button class="date-item">30</button></div>
+                <div class="calendar-date"><button class= "date-item">1</button></div>
+                <div class="calendar-date"><button class= "date-item">2</button></div>
+                <div class="calendar-date"><button class= "date-item">3</button></div>
+                <div class="calendar-date"><button class= "date-item">4</button></div>
+                <div class="calendar-date"><button class= "date-item">5</button></div>
+                <div class="calendar-date"><button class= "date-item">6</button></div>
+                <div class="calendar-date"><button class= "date-item">7</button></div>
+                <div class="calendar-date"><button class= "date-item">8</button></div>
+                <div class="calendar-date"><button class= "date-item">9</button></div>
+                <div class="calendar-date"><button class= "date-item">10</button></div>
+                <div class="calendar-date"><button class= "date-item">11</button></div>
+                <div class="calendar-date"><button class= "date-item">12</button></div>
+                <div class="calendar-date"><button class= "date-item">13</button></div>
+                <div class="calendar-date"><button class= "date-item">14</button></div>
+                <div class="calendar-date"><button class= "date-item">15</button></div>
+                <div class="calendar-date"><button class= "date-item">16</button></div>
+                <div class="calendar-date"><button class= "date-item">17</button></div>
+                <div class="calendar-date"><button class= "date-item">18</button></div>
+                <div class="calendar-date"><button class= "date-item">19</button></div>
+                <div class="calendar-date"><button class= "date-item">20</button></div>
+                <div class="calendar-date"><button class= "date-item">21</button></div>
+                <div class="calendar-date"><button class= "date-item">22</button></div>
+                <div class="calendar-date"><button class= "date-item">23</button></div>
+                <div class="calendar-date"><button class= "date-item">24</button></div>
+                <div class="calendar-date"><button class= "date-item">25</button></div>
+                <div class="calendar-date"><button class= "date-item">26</button></div>
+                <div class="calendar-date"><button class= "date-item">27</button></div>
+                <div class="calendar-date"><button class= "date-item">28</button></div>
+                <div class="calendar-date"><button class= "date-item">29</button></div>
+                <div class="calendar-date"><button class= "date-item">30</button></div>
+                <div class="calendar-date"><button class= "date-item">31</button></div>
+                <div class="calendar-date is-disabled"><button class= "date-item">1</button></div>
+                <div class="calendar-date is-disabled"><button class= "date-item">2</button></div>
+                <div class="calendar-date is-disabled"><button class= "date-item">3</button></div>
+                <div class="calendar-date is-disabled"><button class= "date-item">4</button></div>
+                <div class="calendar-date is-disabled"><button class= "date-item">5</button></div>
+            </div>
+        </div>
+       </div>
+    )
+}
--- src/views/Calendar.js
+++ src/views/Calendar.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-// import DatePicker from "react-datepicker";
+//import DatePicker from "react-datepicker";
 
-// import "react-datepicker/dist/react-datepicker.css";
+//import "react-datepicker/dist/react-datepicker.css";
 import { HeaderComponent } from './../components/ui/Header/HeaderComponent'
+import { PrimaryCalendar } from "../components/ui/Calendars/PrimaryCalendar";
 
 
 const Calendar = () => {
@@ -11,6 +12,7 @@ const Calendar = () => {
     return (
         <div className='container register-content'>
           <HeaderComponent title='Calendario CCAI'/>
+          <PrimaryCalendar/>
         </div>
     )
 }
