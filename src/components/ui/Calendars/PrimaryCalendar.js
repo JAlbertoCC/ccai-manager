04369217bd037bb19ecd632b71a62eaac84c5fc3
WENDY CAIN SOSA <wencain09@gmail.com>
2023-07-08 00:22:44 -0600

    View of the complete modal in the calendar section

--- src/components/ui/Calendars/PrimaryCalendar.js
+++ src/components/ui/Calendars/PrimaryCalendar.js
@@ -1,37 +1,48 @@
-
 import React from 'react';
+import { startOfMonth, getDay } from 'date-fns';
+
 
 export const PrimaryCalendar = (props) => {
-    const {previousMonth, nextMonth, months, currentDate,daysOfWeek} = props
+  const { previousMonth, nextMonth, months, currentDate, daysOfWeek, daysInMonth } = props;
 
-    return (
-       <div className="container">
-        <div className="columns">
-           <div className='column is-three-quarters'>
-            <div className='calendar'>
-                <div className='calendar-nav'>
-                    <button className='button is-text is-small' onClick={previousMonth}>
-                     Anterior
-                    </button>
-                    <button className='button is-text is-small' onClick={nextMonth}>
-                      Siguiente
-                    </button>
-                </div>
-                <div className='calendar-title'>
-                    <h2>{months[currentDate.getMonth()]} {currentDate.getFullYear()}</h2>
-                </div>
+  const daysOfMonth = Array.from({ length: daysInMonth }, (_, index) => index + 1);
+  const firstDayOfMonth = getDay(startOfMonth(currentDate));
+
+
+  return (
+    <div className="container">
+      <div className="columns">
+        <div className="column is-three-quarters">
+          <div className="calendar">
+            <div className="calendar-nav">
+              <button className="button is-text is-small" onClick={previousMonth}>
+                Anterior
+              </button>
+              <button className="button is-text is-small" onClick={nextMonth}>
+                Siguiente
+              </button>
             </div>
-            <div className='calendar-body'>
-                <div className='calendar-weekdays'>
-                    {daysOfWeek.map((day, index) => (
-                        <div className='calendar-weekday' key={index}>{day}</div>
-                    ))}
-                </div>
+            <div className="calendar-title">
+              <h2>{months[currentDate.getMonth()]} {currentDate.getFullYear()}</h2>
             </div>
-           
+          </div>
+          <div className="calendar-body">
+            <div className="calendar-weekdays">
+              {daysOfWeek.map((day, index) => (
+                <div className="calendar-weekday" key={index}>{day}</div>
+              ))}
             </div>
-           </div>
+            <div className="calendar-days">
+              {[...Array(firstDayOfMonth)].map((_, index) => (
+                <div className="calendar-day" key={`empty-${index}`} />
+              ))}
+              {daysOfMonth.map((day) => (
+                <div className="calendar-day" key={day}>{day}</div>
+              ))}
+            </div>
+          </div>
         </div>
-       
-    )
-}
+      </div>
+    </div>
+  );
+};
--- src/views/Calendar.js
+++ src/views/Calendar.js
@@ -1,45 +1,62 @@
 import React, { useState } from "react";
-import ReactDOM from 'react-dom';
-//import DatePicker from "react-datepicker";
-
-//import "react-datepicker/dist/react-datepicker.css";
-
-import { HeaderComponent } from './../components/ui/Header/HeaderComponent'
+import { HeaderComponent } from './../components/ui/Header/HeaderComponent';
 import { PrimaryCalendar } from "../components/ui/Calendars/PrimaryCalendar";
-
 
 const Calendar = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
-  const months = [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Abril', 'Mayo', 'Junio', 
-                   'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
   const daysOfWeek = ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'];
-  
+
   const previousMonth = () => {
-    const previousDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
+    const previousMonth = currentDate.getMonth() - 1;
+    const previousYear = currentDate.getFullYear();
+    let newMonth = previousMonth;
+    let newYear = previousYear;
+
+    if (previousMonth < 0) {
+      newMonth = 11;
+      newYear = previousYear - 1;
+    }
+
+    const daysInMonth = getDaysInMonth(newYear, newMonth);
+    const previousDate = new Date(newYear, newMonth, Math.min(currentDate.getDate(), daysInMonth));
     setCurrentDate(previousDate);
   };
 
   const nextMonth = () => {
-    const nextDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
+    const nextMonth = currentDate.getMonth() + 1;
+    const nextYear = currentDate.getFullYear();
+    let newMonth = nextMonth;
+    let newYear = nextYear;
+
+    if (nextMonth > 11) {
+      newMonth = 0;
+      newYear = nextYear + 1;
+    }
+
+    const daysInMonth = getDaysInMonth(newYear, newMonth);
+    const nextDate = new Date(newYear, newMonth, Math.min(currentDate.getDate(), daysInMonth));
     setCurrentDate(nextDate);
   };
 
- 
+  const getDaysInMonth = (year, month) => {
+    return new Date(year, month + 1, 0).getDate();
+  };
 
-    return (
-        <div className='container register-content'>
-          <HeaderComponent title='Calendario CCAI'/>
-          <PrimaryCalendar 
-             previousMonth={previousMonth}
-             nextMonth={nextMonth}
-             months={months}
-             currentDate={currentDate}
-             daysOfWeek={daysOfWeek}
-             
-          />
-        </div>
-    )
-}
+  return (
+    <div className='container register-content'>
+      <HeaderComponent title='Calendario CCAI' />
+      <PrimaryCalendar
+        previousMonth={previousMonth}
+        nextMonth={nextMonth}
+        months={months}
+        currentDate={currentDate}
+        daysOfWeek={daysOfWeek}
+        daysInMonth={getDaysInMonth(currentDate.getFullYear(), currentDate.getMonth())}
+      />
+    </div>
+  );
+};
 
-
-export default Calendar
+export default Calendar;
